762c9160d2691521d13980d31af41eca840c83c5
Nafeesah <70133267+nafeesah-dev@users.noreply.github.com>
2026-03-03 19:25:50 -0500

    Added Login Page and Login Test using TestNG

---
+++ src/test/java/tests/LoginTests.java
@@ -0,0 +1,31 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import utils.BaseUI;
+import utils.ConfigurationReader;
+import utils.Driver;
+
+public class LoginTests extends BaseUI {
+
+    WebDriver driver = Driver.getDriver();
+    LoginPage loginPage = new LoginPage();
+
+    @Test
+    public void loginTest() {
+
+        loginPage.login(
+                ConfigurationReader.getProperty("username"),
+                ConfigurationReader.getProperty("password")
+        );
+
+        waitForUrlContains("dashboard");
+
+        Assert.assertTrue(
+                driver.getCurrentUrl().contains("dashboard"),
+                "Login failed: Dashboard URL not loaded"
+        );
+    }
+}
